81d8a8246720ed12ad125d718c0a8f5e2eda9bff
vbelavin <belavin@phystech.edu>
2019-12-12 19:40:28 +0300

    noise, damping and external forces

--- double_pendulum/double_pendulum_approximation.py
+++ double_pendulum/double_pendulum_approximation.py
@@ -5,7 +5,17 @@ from torchdiffeq import odeint_adjoint as odeint
 
 
 class DoublePendulumApproxDiffEq(nn.Module):
-    def __init__(self, init, controller=None, mass_1=1., mass_2=1., length_1=1., length_2=1., g=9.8):
+    def __init__(self,
+                 init,
+                 controller=None,
+                 mass_1=1., mass_2=1.,
+                 length_1=1.,
+                 length_2=1.,
+                 damping_1=-0.01,
+                 damping_2=-0.01,
+                 external_force_1=lambda t: 0.,
+                 external_force_2=lambda t: 0.,
+                 g=9.8):
         super().__init__()
         self.mass_1 = mass_1
         self.mass_2 = mass_2
@@ -13,11 +23,14 @@ class DoublePendulumApproxDiffEq(nn.Module):
         self.length_2 = length_2
         self.controller = controller
         self.init = init
+        self.damping_1 = damping_1
+        self.damping_2 = damping_2
+        self.external_force_1 = external_force_1
+        self.external_force_2 = external_force_2
+        self.noise = 1.
         self.g = g
 
     def forward(self, t, x):
-        d_theta_1 = x[:, 2]
-        d_theta_2 = x[:, 3]
         if self.controller is None:
             mass_1, mass_2, length_1, length_2 = self.mass_1, self.mass_2, self.length_1, self.length_2
         else:
@@ -26,19 +39,21 @@ class DoublePendulumApproxDiffEq(nn.Module):
             pred = 2. * (1.01 + self.controller(inp))  # previous and init
             mass_1, mass_2, length_1, length_2 = pred[:, 0], pred[:, 1], pred[:, 2], pred[:, 3]
 
-        M = self.mass_1 + self.mass_2
-        d_phi_1 = (
-                          - self.g * (M + mass_1) * x[:, 0]
-                          - mass_2 * self.g * (x[:, 0] - 2 * x[:, 1])
-                          - 2 * (x[:, 0] - x[:, 1]) * mass_2 * (x[:, 2].pow(2) * length_1 + x[:, 3].pow(2) * length_2)
-                  ) / (
-                          length_1 * (M + mass_1 - mass_2)
-                  )
-        d_phi_2 = (
-                2 * (x[:, 0] - x[:, 1]) * (
-                M * x[:, 2] ** 2 * length_1 + self.g * M + x[:, 3].pow(2) * length_2 * mass_2
-        ) / (
-                        length_2 * (M + mass_1 - mass_2)
-                )
-        )
+        d_theta_1 = x[:, 2]
+        d_theta_2 = x[:, 3]
+        theta_1 = x[:, 0]
+        theta_2 = x[:, 1]
+        delta_theta = theta_2 - theta_1
+        M = mass_1 + mass_2
+
+        right_part_1 = mass_2 * length_2 * d_theta_2.pow(2) * delta_theta - M * self.g * theta_1 + self.damping_1 * d_theta_1 + self.external_force_1(t)
+        denominator_1 = length_1 * (M - mass_2)
+        mul_1 = mass_2
+
+        right_part_2 = - length_1 * d_theta_1.pow(2) * delta_theta - self.g * theta_2 + self.damping_2 * d_theta_2 + self.external_force_2(t)
+        denominator_2 = length_2 * (1 - mass_2 / M)
+        mul_2 = 1. / M
+
+        d_phi_1 = (right_part_1 - right_part_2 * mul_1) / denominator_1
+        d_phi_2 = (right_part_2 - right_part_1 * mul_2) / denominator_2
         return torch.stack([d_theta_1, d_theta_2, d_phi_1, d_phi_2]).t()
--- train.py
+++ train.py
@@ -34,19 +34,36 @@ def plot_pendulums(d1, d2, component=0):
     return fig
 
 
+# TODO: add batch size train / test
+# TODO: controller configurable
+# TODO: rtol/etol?
 @click.command()
 @click.option('--project_name', type=str, prompt='Enter project name')
 @click.option('--work_space', type=str, prompt='Enter workspace name')
 @click.option('--epochs', type=int, default=50000)
 @click.option('--lr', type=float, default=2e-4)
+@click.option('--duration', type=float, default=8)
+@click.option('--step', type=float, default=0.05)
+@click.option('--noise', type=float, default=1e-3)
+@click.option('--logging_period', type=int, default=50)
 @click.option('--method', type=str, default='rk4')
-def main(project_name, work_space, epochs, lr, method):
+def main(
+        project_name: str,
+        work_space: str,
+        epochs: int,
+        lr: float,
+        noise: float,
+        step: float,
+        duration: float,
+        logging_period: int,
+        method: str,
+):
     experiment = Experiment(project_name=project_name, workspace=work_space)
     experiment_key = experiment.get_key()
     PATH = './'
     device = torch.device('cuda:2')
 
-    ts = torch.arange(start=0, end=4, step=0.05).float().to(device)
+    ts = torch.arange(start=0, end=duration, step=step).float().to(device)
     train_inits = torch.clamp(torch.randn(20, 4).float().to(device), -1, 1) / 2.
     test_inits = torch.clamp(torch.randn(20, 4).float().to(device), -1, 1) / 2.
 
@@ -68,36 +85,46 @@ def main(project_name, work_space, epochs, lr, method):
     optimizer = torch.optim.Adam(controller.parameters(), lr=lr, weight_decay=1e-4)
 
     coord_double_pend = odeint(double_pendulum, train_inits, ts, rtol=1e-3, atol=1e-3, method=method).detach().clone()
+    coord_double_pend = coord_double_pend + torch.randn_like(coord_double_pend) * noise
     best_weights = copy.deepcopy(controller.state_dict())
     loss_best = 10000
     for epoch in range(epochs):
         optimizer.zero_grad()
         coord_double_pend_approx = odeint(double_pendulum_approx, train_inits, ts, rtol=1e-3, atol=1e-3, method=method)
         loss = loss_fn(coord_double_pend, coord_double_pend_approx)
-        if loss.item() < loss_best:
-            loss_best = loss.item()
-            best_weights = copy.deepcopy(controller.state_dict())
-            torch.save(best_weights, open(PATH + 'controller_{}.pcl'.format(experiment_key), 'wb+'))
         loss.backward()
         optimizer.step()
 
+        # testing
+        with torch.no_grad():
+            data_pendulum_approx = return_coordinates_double_pendulum(double_pendulum_approx, test_inits, ts)
+            data_pendulum = return_coordinates_double_pendulum(double_pendulum, test_inits, ts)
+            loss_test = np.sqrt(((data_pendulum_approx - data_pendulum) ** 2).mean())
+
+        # saving weights
+        if loss_test.item() < loss_best:
+            loss_best = loss_test.item()
+            print(loss_best, end=' ')
+            best_weights = copy.deepcopy(controller.state_dict())
+            torch.save(best_weights, open(PATH + 'controller_{}.pcl'.format(experiment_key), 'wb+'))
+
         experiment.log_metric('Train loss', loss.item(), step=epoch)
-        if epoch % 5 == 0:
+        experiment.log_metric('Test loss', loss_test, step=epoch)
+
+        # save pics every 50 epochs
+        if epoch % logging_period == 0:
             with torch.no_grad():
                 data_pendulum_approx = return_coordinates_double_pendulum(double_pendulum_approx, test_inits, ts)
                 data_pendulum = return_coordinates_double_pendulum(double_pendulum, test_inits, ts)
-                loss_test = np.sqrt(((data_pendulum_approx - data_pendulum)**2).mean())
-            experiment.log_metric('Test loss', loss_test, step=epoch)
-            fig = plot_pendulums(data_pendulum_approx, data_pendulum)
-            experiment.log_figure("Quality dynamic test", fig, step=epoch)
-            plt.close()
+                fig = plot_pendulums(data_pendulum_approx, data_pendulum)
+                experiment.log_figure("Quality dynamic test", fig, step=epoch)
+                plt.close()
 
-            with torch.no_grad():
                 data_pendulum_approx = return_coordinates_double_pendulum(double_pendulum_approx, train_inits, ts)
                 data_pendulum = return_coordinates_double_pendulum(double_pendulum, train_inits, ts)
-            fig = plot_pendulums(data_pendulum_approx, data_pendulum)
-            experiment.log_figure("Quality dynamic train", fig, step=epoch)
-            plt.close()
+                fig = plot_pendulums(data_pendulum_approx, data_pendulum)
+                experiment.log_figure("Quality dynamic train", fig, step=epoch)
+                plt.close()
 
 
 if __name__ == '__main__':
