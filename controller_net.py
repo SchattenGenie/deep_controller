7ca0d6f10c4479a51d209fdd16924f5b34e54d02
Leonid Gremyachikh <1123qqweaasdzzxc@gmail.com>
2020-01-31 13:35:54 +0300

    add ControllerExternalV1

--- controller_net.py
+++ controller_net.py
@@ -3,16 +3,10 @@ from torch import nn
 
 
 class ControllerV1(nn.Module):
-    def __init__(self, controller_type="internal"):
+    def __init__(self):
         super(ControllerV1, self).__init__()
-        if controller_type == "internal":
-            n_input = 4 + 4 + 1
-        elif controller_type == "external":
-            n_input = 4
-        if controller_type not in ["internal", "external"]:
-            raise ValueError("controller_type should be internal or external")
         self.controller = nn.Sequential(
-            nn.Linear(n_input, 16),
+            nn.Linear(4 + 4 + 1, 16),
             nn.Tanh(),
             nn.Linear(16, 16),
             nn.Tanh(),
@@ -25,3 +19,29 @@ class ControllerV1(nn.Module):
     def forward(self, x):
         x = self.controller(x)
         return torch.exp(x)
+
+
+class ControllerExternalV1(ControllerV1):
+    # TODO: universal Controller for all external controllers
+    def __init__(self):
+        super().__init__()
+        self.controller[0] = nn.Linear(4, 16)
+        self.controller[-1] = nn.Tanh()
+        # TODO: remove Dropout when apply -- add controller.eval() everywhere
+        p = 0.2
+        # self.controller = nn.Sequential(
+        #     nn.Linear(4, 16),
+        #     nn.Dropout(p),
+        #     nn.Tanh(),
+        #     nn.Linear(16, 16),
+        #     nn.Dropout(p),
+        #     nn.Tanh(),
+        #     nn.Linear(16, 16),
+        #     nn.Dropout(p),
+        #     nn.Tanh(),
+        #     nn.Linear(16, 4),
+        #     nn.Tanh()
+        # )
+
+    def forward(self, x):
+        return self.controller(x) * 50  # TODO: limit values with less crutches
