3c53316c9cb144f2559b07f005a89e3bd5011fea
Leonid Gremyachikh <1123qqweaasdzzxc@gmail.com>
2020-02-14 19:07:02 +0300

    plot approx w/o DL

--- vizualization_utils.py
+++ vizualization_utils.py
@@ -4,14 +4,16 @@ import seaborn as sns
 sns.set()
 
 
-def plot_pendulums(d1, d2, component=0):
+def plot_pendulums(d1, d2, d3=None, component=0):
     fig, ax = plt.subplots(2, 2, figsize=(12, 12), dpi=200)
     titles = ["x1", "y1", "x2", "y2"]
     x = np.arange(d1.shape[1])
     for i in range(2):
         for j in range(2):
             ax[i][j].plot(x, d1[i * 2 + j, :, component], label='True')
-            ax[i][j].plot(x, d2[i * 2 + j, :, component], label='Approx')
+            ax[i][j].plot(x, d2[i * 2 + j, :, component], label='Approx with DL')
+            if d3 is not None:
+                ax[i][j].plot(x, d3[i * 2 + j, :, component], label='Approx')
             ax[i][j].set_title(titles[i * 2 + j])
             ax[i][j].legend()
     return fig
